22c61a14fbebeb31c07fbe880ad03108310e2a13
Voosel <111089365+Voosel@users.noreply.github.com>
2022-09-06 21:51:19 +0100

    Add files via upload

---
+++ pole_kola.py
@@ -0,0 +1,24 @@
+#w tym zadaniu liczymy pole koła
+
+wartoscPi= 3.14
+promienOkregu= 5
+poleKola= wartoscPi * promienOkregu**2
+print ("Pole koła wynosi: ",poleKola)
+
+#obwód koła
+obwodKola= wartoscPi * 2 * promienOkregu
+print ("Obwód koła wynosi: ",obwodKola)
+
+#pole prostokąta
+bokA= 5
+bokB= 4
+poleProstokata= bokA * bokB
+print ("Pole prostokąta wynosi: ",poleProstokata)
+
+#pole trapezu
+bokTrapezuA= 5
+bokTrapezuB= 4
+wysokoscTrapezu = 3
+
+poleTrapezu= (bokTrapezuA+bokTrapezuB)*wysokoscTrapezu /2
+print("Pole trapezu wynosi: ",poleTrapezu)
