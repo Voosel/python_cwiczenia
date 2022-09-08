5db051c0dd61b63b04944dfe46645bca9b48323c
Patryk Stajniak <velkan88@gmail.com>
2022-09-08 22:35:58 +0200

    Commit ze szkoleniami Pythona - Stringi

---
+++ string2.py
@@ -0,0 +1,17 @@
+#nazwisko
+firstName='Kasia'
+familyName='Sowa'
+lastName='Mrugała'
+
+newName= firstName+' '+familyName+' '+lastName
+print(newName)
+
+#tytuły piosenek
+music='"Universal Fanfare" Jerry Goldsmith \n"Happy Together" Garry Bonner \n"I\'m a Man" Steve Winwood'
+print(music)
+
+#królik
+
+print('(\(\\')
+print("(-.-)")
+print('0_(")(")')
---
+++ string3.py
@@ -0,0 +1,4 @@
+#monty python
+article='''Monty Python (also collectively known as the Pythons)[2][3] were a British surreal comedy troupe who created the sketch comedy television show Monty Python's Flying Circus, which first aired on the BBC in 1969. Forty-five episodes were made over four series. The Python phenomenon developed from the television series into something larger in scope and influence, including touring stage shows, films, albums, books and musicals. The Pythons' influence on comedy has been compared to the Beatles'''
+print(article.upper())
+print(article.lower().replace("monty","flying"))
